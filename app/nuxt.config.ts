dc30a6a8677cb54ffafb12afa7cc228cad59c2bc
Ted Son <ted.son@atlaslabs.ai>
2023-11-18 05:14:13 +0900

    🔧 Stricter type checks

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  typescript: { strict: true },
   css: ["bootstrap/dist/css/bootstrap.min.css", "bootstrap-icons/font/bootstrap-icons.css"],
   build: {
     transpile: ["trpc-nuxt"],
